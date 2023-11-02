dbe21ed69e3c19fa35ffb3e12959ebf8c42fe492
c0ndi <kowalik0konrad@gmail.com>
2023-11-03 00:18:07 +0100

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+        <Script strategy="afterInteractive" id="" dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -13,7 +13,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-K56DL568');`}}></Script>
       </Head>
       <body>
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K56DL568"
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K56DL568"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         <Main />
         <NextScript />
