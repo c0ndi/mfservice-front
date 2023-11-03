c54bfe6f7334ee1e1f0f34abe3b1f6b32662f0c7
c0ndi <kowalik0konrad@gmail.com>
2023-11-03 12:56:51 +0100

    fix

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://harley-davidson-legends.pl",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["13.49.159.208", "localhost", "46.101.147.9", "strapi.harley-heaven-poland.pl"]
+    domains: [
+      "13.49.159.208",
+      "localhost",
+      "46.101.147.9",
+      "strapi.harley-heaven-poland.pl",
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
